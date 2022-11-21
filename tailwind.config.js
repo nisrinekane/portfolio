2b219cc930138133b33588c907abc54d4ae3066e
Nisrine Kane <34342007+nisrinekane@users.noreply.github.com>
2022-11-20 17:44:23 -0800

    Added projects, changed logo, changed styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,9 @@ module.exports = {
       colors: {
         "custom-white": "#ccd6f6",
         "custom-gray":"#8892b0",
-        "custom-blue": "#112240"
+        "custom-blue": "#112240",
+        "custom-green": "#64ffd9",
+        "custom-bg": "#0a192f"
       },
       body: {
         padding: "50px"
