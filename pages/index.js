1bd7fd5dd9a6b473da4c02363766c5c42c8860b0
Nisrine Kane <34342007+nisrinekane@users.noreply.github.com>
2022-11-21 18:11:58 -0800

    added build

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
             </div>
             <ul className="flex items-center">
               <li>
-                <a className="bg-gradient-to-r from-teal-500 text- to-cyan-500 text-white px-4 py-2 border-none rounded-md ml-8" href="https://drive.google.com/file/d/1PqPiFZbvkqCaY-VadtUdZnpCBjnDPLgI/view?usp=sharing" target='_blank' rel='noopener noreferrer'> Resume </a>
+                <a className="bg-gradient-to-r from-teal-500 text- to-cyan-500 text-white px-4 py-2 border-none rounded-md ml-8" href="https://drive.google.com/file/d/1j1rjHpo0BT8gGENXfzB5NQCTr2IOQQhg/view?usp=sharing" target='_blank' rel='noopener noreferrer'> Resume </a>
               </li>
             </ul>
           </nav>
